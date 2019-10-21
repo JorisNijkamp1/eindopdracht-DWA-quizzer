e21e91f3cb2b5606dabb7a41548f1d014b367657
Joris <jorisnijkamp@gmail.com>
2019-10-21 13:33:29 +0200

    titel

--- quizzer/src/App.js
+++ quizzer/src/App.js
@@ -11,19 +11,22 @@ import Button from "react-bootstrap/Button";
 import Nav from "react-bootstrap/Nav";
 
 function App() {
-  return (
-      <Container>
-          <Row>
-              <Col md={{ span: 4, offset: 4 }} fluid className="h-100">
-                  <Nav defaultActiveKey="/home" className="flex-column text-center min-vh-100 justify-content-center">
-                      <Nav.Link className="btn btn-outline-primary">Quiz master</Nav.Link>
-                      <Nav.Link eventKey="link-1" className="btn btn-outline-success">Team</Nav.Link>
-                      <Nav.Link eventKey="link-2" className="btn btn-outline-info">Scoreboard</Nav.Link>
-                  </Nav>
-              </Col>
-          </Row>
-      </Container>
-  );
+    return (
+        <Container>
+            <Row className="min-vh-100">
+                <Col md={{span: 8, offset: 2}} fluid>
+                    <h1 className="text-center display-1">Quizzer Night</h1>
+                </Col>
+                <Col md={{span: 4, offset: 4}} fluid>
+                    <Nav defaultActiveKey="/home" className="flex-column text-center justify-content-center">
+                        <Nav.Link eventKey="link-1" className="btn btn-outline-primary">Quiz master</Nav.Link>
+                        <Nav.Link eventKey="link-2" className="btn btn-outline-success">Team</Nav.Link>
+                        <Nav.Link eventKey="link-3" className="btn btn-outline-info">Scoreboard</Nav.Link>
+                    </Nav>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 export default App;
