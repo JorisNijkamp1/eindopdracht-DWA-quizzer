64bfadab9cfe748cf80f03f528cab6ef6f8b5dd7
Joris <jorisnijkamp@gmail.com>
2019-10-21 13:38:35 +0200

    titel

--- quizzer/src/components/GameMenu.jsx
+++ quizzer/src/components/GameMenu.jsx
@@ -3,13 +3,16 @@ import Col from "react-bootstrap/Col";
 import Nav from "react-bootstrap/Nav";
 import React from "react";
 import Container from "react-bootstrap/Container";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 class GameMenu extends React.Component {
     render() {
         return (
             <Container>
                 <Row>
+                    <Col md={{span: 8, offset: 2}}>
+                        <h1 className="text-center display-1">Quizzer Night</h1>
+                    </Col>
                     <Col md={{span: 4, offset: 4}} className="h-100">
                         <Nav defaultActiveKey="/home"
                              className="flex-column text-center min-vh-100 justify-content-center">
