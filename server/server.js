c071020348748a93a448be3ae2f3edeca53bb428
Aaron van den Berg <a.vdberg98@gmail.com>
2019-10-28 12:14:51 +0100

    Kleine bug voor button start game gefixt

--- server/server.js
+++ server/server.js
@@ -127,6 +127,7 @@ app.post('/api/game', async (req, res) => {
         //create gameRoomName
         var newGame = new Games({
             _id: gameRoomName,
+            game_status: 'lobby'
         });
 
         //save gameRoomName document to MongoDB
