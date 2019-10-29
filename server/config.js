f713f1dd018452b46f46f8c3cf3bb72b884d889e
Aaron van den Berg <a.vdberg98@gmail.com>
2019-10-29 12:06:18 +0100

    Db updated

--- server/config.js
+++ server/config.js
@@ -1,16 +1,8 @@
-//ONLINE DEVELOPMENT
-const USERNAME = "root";
-const PASSWORD = "yQe3nqgFnIihFVRaGqX0SrPo";
-const HOST = "81.169.201.22";
-const PORT = "27017";
-const DB = "admin";
-
-
 //LOCALHOST DEVELOPMENT
-//const USERNAME = "";
-//const PASSWORD = "";
-//const HOST = "localhost";
-//const PORT = "27017";
-//const DB = "quizzer";
+const USERNAME = "";
+const PASSWORD = "";
+const HOST = "localhost";
+const PORT = "27017";
+const DB = "quizzer";
 
 var exports = module.exports = {USERNAME, PASSWORD, HOST, PORT, DB};
